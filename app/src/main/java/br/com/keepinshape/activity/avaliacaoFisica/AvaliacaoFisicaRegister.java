d5cc14a0e80195f0f2642e8fc8a52373dd63e0d5
root <joao_nad@hotmail.com>
2016-02-11 17:20:00 -0200

    Adicionado Activity para registro da Avaliação Física.

---
+++ app/src/main/java/br/com/keepinshape/activity/avaliacaoFisica/AvaliacaoFisicaRegister.java
@@ -0,0 +1,16 @@
+package br.com.keepinshape.activity.avaliacaoFisica;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import br.com.keepinshape.R;
+
+public class AvaliacaoFisicaRegister extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao_fisica_register);
+    }
+
+}
